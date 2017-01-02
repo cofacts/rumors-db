e22d25a560a1ef129c7591803c7d907367d167c0
MrOrz <johnsonliang7@gmail.com>
2017-01-02 21:27:05 +0800

    Remove unused airtable stuff

--- config/default.js
+++ config/default.js
@@ -1,11 +1,5 @@
 module.exports = {
   // elasticsearch setup
   //
-  ELASTICSEARCH_URL: 'http://localhost:9200',
-
-  // Airtable setup
-  //
-  AIRTABLE_BASE: 'apphrXta7kRli978O',
-  AIRTABLE_VIEW: 'Export',
-  AIRTABLE_APIKEY: '<THE_KEY>',
+  ELASTICSEARCH_URL: 'http://localhost:62222',
 };
