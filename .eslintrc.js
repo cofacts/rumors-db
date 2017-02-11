55551b1ae456a049a614f7da09dab8d0148add01
MrOrz <johnsonliang7@gmail.com>
2017-02-12 01:11:22 +0800

    Remove annoying lint warning about console.xxx

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'no-underscore-dangle': 'off', // elastic search results uses dangling underscore a lot (like _source)
     'no-param-reassign': 'off', // ctx.body = xxx is how koa2 works.
     'no-restricted-syntax': 'off', // we use for-of along with await.
+    'no-console': 'off' // It's seed script! We use no-console a hell lot.
   },
   settings: {
     'import/resolver': {
