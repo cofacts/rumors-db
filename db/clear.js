875e78a1052f74932b2e27c017fb29ddb5bf2a36
MrOrz <johnsonliang7@gmail.com>
2017-12-26 00:45:20 +0800

    Update `npm run clear` to really clear all indices

--- db/clear.js
+++ db/clear.js
@@ -2,20 +2,11 @@ import config from 'config';
 import elasticsearch from 'elasticsearch';
 import '../util/catchUnhandledRejection';
 
-import * as schema from '../schema';
-
 const client = new elasticsearch.Client({
   host: config.get('ELASTICSEARCH_URL'),
   log: 'trace',
 });
 
-Object.keys(schema).forEach(index => {
-  const indexName = `${index}_v*`;
-  client.indices
-    .delete({
-      index: indexName,
-    })
-    .then(() => {
-      console.log(`Index "${indexName}" deleted.`);
-    });
+client.indices.delete({ index: '_all' }).then(() => {
+  console.log(`All indices has been deleted.`);
 });
