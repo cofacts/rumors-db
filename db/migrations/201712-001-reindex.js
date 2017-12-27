215510930ef6b88433a3940a50bf6266fc7dcb88
MrOrz <johnsonliang7@gmail.com>
2017-12-27 23:01:18 +0800

    Add replies reindexing script

--- db/migrations/201712-001-reindex.js
+++ db/migrations/201712-001-reindex.js
@@ -1,3 +1,4 @@
+import '../../util/catchUnhandledRejection';
 import config from 'config';
 import elasticsearch from 'elasticsearch';
 
@@ -8,27 +9,57 @@ const client = new elasticsearch.Client({
   log: 'trace',
 });
 
-client.reindex({
-  waitForCompletion: true,
-  body: {
-    source: { index: 'articles_legacy' },
-    dest: {
-      index: getIndexName('articles'),
-      type: 'doc',
-      op_type: 'create',
-    },
-    conflicts: 'proceed',
-    script: {
-      lang: 'painless',
-      source: `
-        ctx._source.put('articleReplies', new ArrayList());
-        ctx._source.put('hyperlinks', new ArrayList());
-        ctx._source.put('tags', new ArrayList());
-        ctx._source.put('replyRequestCount', ctx._source.replyRequestIds.size());
+async function main() {
 
-        ctx._source.put('app_id', ctx._source.from);
-        ctx._source.remove('from');
-      `,
+  client.reindex({
+    waitForCompletion: true,
+    body: {
+      source: { index: 'articles_legacy' },
+      dest: {
+        index: getIndexName('articles'),
+        type: 'doc',
+        op_type: 'create',
+      },
+      conflicts: 'proceed',
+      script: {
+        lang: 'painless',
+        source: `
+          ctx._source.put('articleReplies', new ArrayList());
+          ctx._source.put('hyperlinks', new ArrayList());
+          ctx._source.put('tags', new ArrayList());
+          ctx._source.put('replyRequestCount', ctx._source.replyRequestIds.size());
+
+          ctx._source.put('app_id', ctx._source.from);
+          ctx._source.remove('from');
+        `,
+      },
     },
-  },
-});
+  });
+
+  client.reindex({
+    waitForCompletion: true,
+    body: {
+      source: { index: 'replies_legacy' },
+      dest: {
+        index: getIndexName('replies'),
+        type: 'doc',
+        op_type: 'create',
+      },
+      conflicts: 'proceed',
+      script: {
+        lang: 'painless',
+        source: `
+          ctx._source.put('hyperlinks', new ArrayList());
+
+          ctx._source.putAll(ctx._source.versions[0]);
+          ctx._source.remove('versions');
+
+          ctx._source.put('app_id', ctx._source.from);
+          ctx._source.remove('from');
+        `,
+      },
+    },
+  });
+}
+
+main();
