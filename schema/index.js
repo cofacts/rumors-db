daf4b4ccfb08773457e303911df9633c9875f276
ggm <godgunman@gmail.com>
2020-02-28 20:14:31 +0800

    Remove tags

--- schema/index.js
+++ schema/index.js
@@ -2,16 +2,7 @@ import { default as articles } from './articles';
 import { default as replyrequests } from './replyrequests';
 import { default as articlereplyfeedbacks } from './articlereplyfeedbacks';
 import { default as replies } from './replies';
-import { default as tags } from './tags';
 import { default as users } from './users';
 import { default as urls } from './urls';
 
-export {
-  articles,
-  replyrequests,
-  articlereplyfeedbacks,
-  replies,
-  tags,
-  users,
-  urls,
-};
+export { articles, replyrequests, articlereplyfeedbacks, replies, users, urls };
