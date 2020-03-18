8467aa010115ef92c37e5b457292037c5a4a9b74
ggm <godgunman@gmail.com>
2020-03-18 23:32:45 +0800

    Fix missed export

--- schema/index.js
+++ schema/index.js
@@ -1,8 +1,19 @@
 import { default as articles } from './articles';
 import { default as replyrequests } from './replyrequests';
 import { default as articlereplyfeedbacks } from './articlereplyfeedbacks';
+import { default as articlecategoryfeedbacks } from './articlecategoryfeedbacks';
 import { default as replies } from './replies';
+import { default as categories } from './categories';
 import { default as users } from './users';
 import { default as urls } from './urls';
 
-export { articles, replyrequests, articlereplyfeedbacks, replies, users, urls };
+export {
+  articles,
+  replyrequests,
+  articlereplyfeedbacks,
+  articlecategoryfeedbacks,
+  replies,
+  categories,
+  users,
+  urls,
+};
