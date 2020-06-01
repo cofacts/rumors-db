db76036387627442faf22d86ee376beda37ea7bd
MrOrz <johnsonliang7@gmail.com>
2020-06-02 01:09:05 +0800

    Introduce browser-app and backend-app users

--- schema/users.js
+++ schema/users.js
@@ -1,14 +1,24 @@
 export default {
   properties: {
-    email: { type: 'keyword' },
     name: { type: 'keyword' },
     avatarUrl: { type: 'keyword' },
 
+    createdAt: { type: 'date' },
+    updatedAt: { type: 'date' },
+
+    // Timestamp for the last time the user performed a rumors-api API call
+    lastActiveAt: { type: 'date' },
+
+    // Only for users created by browser apps, managed by passport.js.
+    //
+    email: { type: 'keyword' },
     facebookId: { type: 'keyword' },
     githubId: { type: 'keyword' },
     twitterId: { type: 'keyword' },
 
-    createdAt: { type: 'date' },
-    updatedAt: { type: 'date' },
+    // Only for users created by backend app, managed by the app itself.
+    //
+    appUserId: { type: 'keyword' },
+    appId: { type: 'keyword' },
   },
 };
