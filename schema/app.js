0b769cbf6d072c80c417ee85f2ca72f459a81ac8
MrOrz <johnsonliang7@gmail.com>
2020-06-02 01:20:24 +0800

    Adjust app mappings

--- schema/app.js
+++ schema/app.js
@@ -2,9 +2,13 @@ export default {
   _all: { enabled: false },
   properties: {
     adminUserId: { type: 'keyword' },
+    title: { type: 'text' }, // User-readable name for the app
+    description: { type: 'text', analyzer: 'cjk' }, // intro for the app
 
     secret: { type: 'keyword' }, // for backend apps
-    redirectUrl: { type: 'keyword' }, // for in-browser apps
+
+    // for browser apps
+    corsOrigins: { type: 'keyword' }, // can have multiple
 
     createdAt: { type: 'date' },
     updatedAt: { type: 'date' },
