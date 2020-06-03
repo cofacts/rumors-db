8a18193304d0f7fd188225c049f0f629aa56f14d
Johnson Liang <johnson.liang@appier.com>
2020-06-03 13:56:10 +0800

    Kickstart scroll & bulk processing

--- db/migrations/202006-001-rewrite-userids.js
+++ db/migrations/202006-001-rewrite-userids.js
@@ -7,42 +7,151 @@
 
 import 'dotenv/config';
 import '../../util/catchUnhandledRejection';
+import crypto from 'crypto';
 import elasticsearch from '@elastic/elasticsearch';
 
 const client = new elasticsearch.Client({
   node: process.env.ELASTICSEARCH_URL,
 });
 
-/**
- * https://github.com/elastic/elasticsearch/issues/30790
- * https://github.com/elastic/elasticsearch/issues/55352
- * https://discuss.elastic.co/t/how-do-you-use-the-examplewhitelistextension-in-terms-aggregations/120551/7
- */
-async function rewriteUserIdsInArticles() {
-  // await client
-  //   .updateByQuery({
-  //     index: 'articles',
-  //     type: 'doc',
-  //     body: {
-  //       script: {
-  //         lang: 'painless',
-  //         source: `
+const BATCH_SIZE = 100;
+const SCROLL_TIMEOUT = '30s';
 
-  //           if (ctx._source.articleCategories === null) {
-  //             ctx._source.articleCategories = new ArrayList();
-  //           } else {
-  //             ctx.op = 'noop';
-  //           }
-  //         `,
-  //       },
-  //     },
-  //   })
-  //   .then(resp => console.log(resp))
-  //   .catch(e => console.error(JSON.stringify(e, null, '  ')));
+/**
+ * Utils from rumors-api
+ */
+/**
+ * @param {string} appId - app ID
+ * @param {string} rawUserId - raw user ID given by an backend app
+ * @returns {string} the user ID in `users` index if rumors-db
+ */
+function getBackendAppUserIdInDb(appId, rawUserId) {
+  const hash = crypto.createHash('sha256');
+  hash.update(`${appId}|${rawUserId}`);
+  return hash.digest('base64');
+}
+
+/**
+ * Fetches all docs in the specified index.
+ *
+ * @param {String} indexName The name of the index to fetch
+ * @yields {Object} the document
+ */
+async function* getAllDocs(indexName) {
+  let resp = await client.search({
+    index: indexName,
+    scroll: SCROLL_TIMEOUT,
+    size: BATCH_SIZE,
+    body: {
+      query: {
+        match_all: {},
+      },
+    },
+  });
+
+  while (true) {
+    const docs = resp.body.hits.hits;
+    if (docs.length === 0) break;
+
+    for (const doc of docs) {
+      yield doc;
+    }
+
+    if (!resp.body._scroll_id) {
+      break;
+    }
+
+    resp = await client.scroll({
+      scroll: SCROLL_TIMEOUT,
+      scrollId: resp.body._scroll_id,
+    });
+  }
+}
+
+class Bulk {
+  _operations = [];
+
+  push(...args) {
+    this._operations.push(...args);
+    if (this._operations.length >= BATCH_SIZE) {
+      this.flush();
+    }
+
+    return this;
+  }
+
+  async flush() {
+    // Clean up _operations now
+    const op = this._operations;
+    this._operations = [];
+
+    const { body } = await client.bulk({
+      body: op,
+    });
+    if (body.errors) {
+      console.error(body.errors);
+    } else {
+      console.info(
+        'Bulk operations complete:',
+
+        // Count each results
+        body.items.reduce((results, { result }) => {
+          if (results[result] === undefined) results[result] = 0;
+          results[result] += 1;
+        }, {})
+      );
+    }
+
+    return this;
+  }
+}
+
+const bulk = new Bulk();
+
+async function rewriteUserIdsInReplyRequests() {
+  for await (const doc of getAllDocs('replyrequests')) {
+    const backendAppUserId = getBackendAppUserIdInDb(doc.appId, doc.userId);
+    bulk.push(
+      {
+        delete: {
+          _index: 'replyrequests',
+          _type: 'doc',
+          _id: doc._id,
+        },
+      },
+      {
+        index: {
+          _index: 'replyrequests',
+          _type: 'doc',
+          _id: `${doc.articleId}__${backendAppUserId}`,
+        },
+      }
+    );
+  }
+}
+
+async function rewriteUserIdsInArticles() {
+  for await (const doc of getAllDocs('articles')) {
+    bulk.push(
+      {
+        update: {
+          _index: 'articles',
+          _type: 'doc',
+          _id: doc._id,
+        },
+      },
+      {
+        doc: {
+          references: [],
+        },
+      }
+    );
+  }
 }
 
 async function main() {
   await rewriteUserIdsInArticles();
+  await rewriteUserIdsInReplyRequests();
 }
 
 main();
