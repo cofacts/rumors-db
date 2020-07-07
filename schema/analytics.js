ce948a43bc0c1752fc3d27f1c848596869c48f2d
Zoe <1817638+ztsai@users.noreply.github.com>
2020-07-07 21:10:48 +0800

    add schema for analytics

---
+++ schema/analytics.js
@@ -0,0 +1,24 @@
+export default {
+  properties: {
+    date: { type: 'date' },
+
+    // article id or reply id
+    docId: { type: 'keyword' },
+
+    // the timestamp for when the data is fetched
+    fetchedAt: { type: 'date' },
+
+    stats: {
+      type: 'object',
+      properties: {
+        lineUser: { type: 'long' },
+        lineVisit: { type: 'long' },
+        webUser: { type: 'long' },
+        webVisit: { type: 'long' },
+      },
+    },
+
+    // either article or reply
+    type: { type: 'keyword' }
+  }
+};
