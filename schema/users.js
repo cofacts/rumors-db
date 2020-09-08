9f875672915ad1d573adf31e66094daef77a47b7
Zoe <1817638+ztsai@users.noreply.github.com>
2020-09-08 18:07:58 +0800

    comments

--- schema/users.js
+++ schema/users.js
@@ -3,8 +3,12 @@ export default {
     email: { type: 'keyword' },
     name: { type: 'keyword' },
     avatarUrl: { type: 'keyword' },
-    avatarType: { type: 'keyword' },
-    avatarData: { type: 'text' },
+
+    avatarType: { type: 'keyword' }, // url, gravatar, openpeeps
+    avatarData: {
+      type: 'text',
+      index: false,
+    }, // could be url or json props for openpeeps
 
     appId: { type: 'keyword' },
     appUserId: { type: 'keyword' },
