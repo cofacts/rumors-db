650c9cab5cb0422cf73aaa8d808cc62b82764175
MrOrz <johnsonliang7@gmail.com>
2021-12-08 02:02:09 +0800

    Add new fields to categories

--- schema/categories.js
+++ schema/categories.js
@@ -3,8 +3,11 @@ export const VERSION = '1.1.0';
 export default {
   properties: {
     title: { type: 'text', analyzer: 'cjk' },
+    target: { type: 'text', analyzer: 'cjk' }, // The target editors. Separated by line breaks.
     description: { type: 'text', analyzer: 'cjk' },
     createdAt: { type: 'date' },
     updatedAt: { type: 'date' },
+    status: { type: 'keyword' }, // NORMAL, DELETED
+    parentId: { type: 'keywords' }, // Parent category's ID
   },
 };
