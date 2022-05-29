a9604adeae72a4543469edf001ebb4acd76f8829
MrOrz <johnsonliang7@gmail.com>
2022-05-29 14:39:18 +0800

    Add author ids in articlereplyfeedbacks

--- schema/articlereplyfeedbacks.js
+++ schema/articlereplyfeedbacks.js
@@ -1,4 +1,4 @@
-export const VERSION = '1.1.1';
+export const VERSION = '1.2.0';
 
 export default {
   properties: {
@@ -11,6 +11,12 @@ export default {
     // The user submits the feedback with which client.
     // Should be one of backend APP ID, 'BOT_LEGACY', 'RUMORS_LINE_BOT' or 'WEBSITE'
     appId: { type: 'keyword' },
+
+    // `users` index ID for the author of the reply
+    replyUserId: { type: 'keyword' },
+
+    // `users` index ID for the author of the article-reply
+    articleReplyUserId: { type: 'keyword' },
 
     score: { type: 'byte' }, // 1, 0, -1
     comment: { type: 'text', analyzer: 'cjk_url_email' }, // user comment for the article reply
