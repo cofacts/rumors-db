d10b07b3cf52a1102f8c4bc21da85daa03f83618
Nonumpa <acerxp511@msn.com>
2023-09-07 12:55:02 +0800

    feat(collab): add ydoc versions

--- schema/ydocs.js
+++ schema/ydocs.js
@@ -1,7 +1,14 @@
-export const VERSION = '1.0.0';
+export const VERSION = '1.0.1';
 
 export default {
   properties: {
     ydoc: { type: 'binary' },
+    versions: {
+      type: 'nested',
+      properties: {
+        snapshot: { type: 'binary' },
+        createdAt: { type: 'date' },
+      },
+    },
   },
 };
