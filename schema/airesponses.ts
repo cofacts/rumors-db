ca31378324d9521baaa9226e8bbafc9bbcf9dd3a
MrOrz <johnsonliang7@gmail.com>
2024-05-08 22:18:54 +0800

    feat: add real example in airesponses

--- schema/airesponses.ts
+++ schema/airesponses.ts
@@ -20,7 +20,7 @@ export type AIResponse = {
   text?: string;
 
   /** The request to AI endpoint. Just for record, not indexed. */
-  request: string;
+  request?: string;
 
   /** Token stats from AI endpoint response */
   usage?: {
@@ -34,32 +34,39 @@ export type AIResponse = {
 };
 
 export const examples: AIResponse[] = [
-  // Loading AI response; no text and usage yet
+  // AI response example
   {
-  docId: '1',
-  type: 'AI_REPLY',
-  userId: '1',
-  appId: '1',
-  status: 'LOADING',
-  request: '{"params": "Hello, World!"}',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-}, {
-  docId: '2',
-  type: 'TRANSCRIPT',
-  userId: '1',
-  appId: '1',
-  status: 'SUCCESS',
-  text: 'Hello, World!',
-  request: '{"params": "Hello, World!"}',
-  usage: {
-    promptTokens: 2,
-    completionTokens: 2,
-    totalTokens: 4,
+    userId: 'some-user-id',
+    appId: 'line-bot',
+    docId: '28d15z6t2y499',
+    type: 'AI_REPLY',
+    status: 'SUCCESS',
+    request: `{"model":"gpt-3.5-turbo","messages":[{"role":"system","content":"今天是2023年4月1日。你是協助讀者進行媒體識讀的小幫手。你說話時總是使用台灣繁體中文。有讀者傳了一則網路訊息給你。"},{"role":"user","content":"馬英九的祭文如下\n\n親愛的立安公公：您好！我是英九，您老人家的孫兒，爸爸鶴凌公的長子。"},{"role":"user","content":"請問作為閱聽人，我應該注意這則訊息的哪些地方呢？\n請節錄訊息中需要特別留意的地方，說明為何閱聽人需要注意它，謝謝。"}]}`,
+    createdAt: new Date('2023-04-01T22:26:16.225Z'),
+    usage: {
+      promptTokens: 1531,
+      totalTokens: 1876,
+      completionTokens: 345,
+    },
+    text: `
+1. 資訊來源：這篇訊息的資訊來源需要確認是否可信。例如，作者的身分是否可考，訊息的發佈平台是否可信等。如果無法確認訊息來源，則需要小心評估訊息的可信度。
+
+2. 時間：訊息中提到了目前的日期以及歷史事件，需要注意其是否與實際情況相符。例如，訊息提到的日期是否正確，歷史事件的時間、地點等是否符合事實。
+`,
+    updatedAt: new Date('2023-04-01T22:26:34.227Z'),
   },
-  createdAt: new Date(),
-  updatedAt: new Date(),
-}];
+  // Transcript example
+  {
+    userId: 'some-user-id',
+    appId: 'line-bot',
+    status: 'SUCCESS',
+    createdAt: new Date('2023-09-02T16:27:30.167Z'),
+    type: 'TRANSCRIPT',
+    docId: 'video.j4v21_q-3k4sIGXTda6J32MhJRXTIqs9GVCpZBfYdxM',
+    text: '鬧大了。俄羅斯在聯合國公開宣稱武力介入日本核污染問題。',
+    updatedAt: new Date('2023-09-02T16:27:41.883Z'),
+  },
+];
 
 export default {
   properties: {
@@ -89,4 +96,3 @@ export default {
     updatedAt: { type: 'date' },
   },
 };
-
